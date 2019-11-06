c75cbc9a474895898971c32c92c1acc39ee1aafb
YeonwooSung <neos960518@gmail.com>
2019-11-07 05:34:32 +0900

    Add TODO

--- src/main/java/modelChecker/SimpleModelChecker.java
+++ src/main/java/modelChecker/SimpleModelChecker.java
@@ -2,12 +2,16 @@ package modelChecker;
 
 import formula.stateFormula.StateFormula;
 import model.Model;
+import model.State;
 
 public class SimpleModelChecker implements ModelChecker {
 
     @Override
     public boolean check(Model model, StateFormula constraint, StateFormula query) {
-        // TODO Auto-generated method stub
+        State[] initStates = model.getStates(); //get the initial states
+        //TODO get paths (generate path tree? or do something else?) -> check all states?
+        //TODO check the initial states
+
         return false;
     }
 
